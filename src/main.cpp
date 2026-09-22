a25de06c162568e4e5198774240495b7ac5ff68d
mahuahyl <2708526833@qq.com>
2026-09-22 23:25:42 +0800

    完善角色死亡，拥有完整游戏流程

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@
 #include "enemy.h"
 #include "scorer.h"
 
+float cal_distant(sf::Vector2f p1, sf::Vector2f p2)
+{
+    float dx, dy;
+    dx = p1.x - p2.x;
+    dy = p1.y - p2.y;
+    return (dx * dx + dy * dy);
+}
+
 int main()
 {
     // 窗口设置
@@ -25,6 +33,14 @@ int main()
     std::mt19937 gen(rd());
     std::uniform_real_distribution<float> ydist(0.f, 600.f);
     std::uniform_real_distribution<float> xdist(0.f, 800.f);
+
+    // 游戏状态机
+    enum class game_state
+    {
+        playing,
+        over
+    };
+    game_state state = game_state::playing;
 
     // 1. 载入纹理
     sf::Texture p_run_texture, p_idle_texture, npc_run_texture, npc_idle_texture, enemy_idle_texture, enemy_run_texture, bullet_texture, text_texture;
@@ -83,22 +99,25 @@ int main()
         float dt = clock.restart().asSeconds();
 
         // 敌人生成
-        spawnTimer += dt;
-        if (spawnTimer > spawnInterval)
+        if (state == game_state::playing)
         {
-            sf::Vector2f position;
-            sf::Vector2f dis;
-            do
+            spawnTimer += dt;
+            if (spawnTimer > spawnInterval)
             {
+                sf::Vector2f position;
+                sf::Vector2f dis;
+                do
+                {
 
-                position = {xdist(gen), ydist(gen)};
-                dis = position - p1.getPosition();
+                    position = {xdist(gen), ydist(gen)};
+                    dis = position - p1.getPosition();
 
-            } while (std::hypot(dis.x, dis.y) < 50.f);
+                } while (std::hypot(dis.x, dis.y) < 50.f);
 
-            Enemy e1(enemy_idle_texture, enemy_run_texture, &p1, position);
-            enemies.push_back(e1);
-            spawnTimer -= spawnInterval;
+                Enemy e1(enemy_idle_texture, enemy_run_texture, &p1, position);
+                enemies.push_back(e1);
+                spawnTimer -= spawnInterval;
+            }
         }
 
         // 事件检测
@@ -107,71 +126,102 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
 
-            if (const auto *mouse = event->getIf<sf::Event::MouseButtonPressed>())
-            {
-                if (mouse->button == sf::Mouse::Button::Left)
-                {
-                    sf::Vector2f mouse_pos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
-                    sf::Vector2f b_dire = mouse_pos - p1.getPosition();
+            if (state == game_state::playing)
 
-                    bullets.emplace_back(bullet_texture, b_dire, p1.getPosition());
+            {
+                if (const auto *mouse = event->getIf<sf::Event::MouseButtonPressed>())
+                {
+                    if (mouse->button == sf::Mouse::Button::Left)
+                    {
+                        sf::Vector2f mouse_pos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
+                        sf::Vector2f b_dire = mouse_pos - p1.getPosition();
+
+                        bullets.emplace_back(bullet_texture, b_dire, p1.getPosition());
+                    }
                 }
-            }
 
-            if (const auto *key = event->getIf<sf::Event::KeyPressed>())
-            {
-                if (key->code == sf::Keyboard::Key::P)
+                if (const auto *key = event->getIf<sf::Event::KeyPressed>())
                 {
-                    Enemy e1(enemy_idle_texture, enemy_run_texture, &p1, {100.f, 200.f});
-                    enemies.push_back(e1);
+                    if (key->code == sf::Keyboard::Key::P)
+                    {
+                        Enemy e1(enemy_idle_texture, enemy_run_texture, &p1, {100.f, 200.f});
+                        enemies.push_back(e1);
+                    }
                 }
             }
         }
 
         // 敌人和子弹行动
-        for (auto &it : bullets)
+        if (state == game_state::playing)
         {
-            it.update(dt);
-        }
-        for (auto &it : enemies)
-        {
-            it.update(dt);
-        }
+            for (auto &it : bullets)
+            {
+                it.update(dt);
+            }
+            for (auto &it : enemies)
+            {
+                it.update(dt);
+            }
 
-        for (auto &b_it : bullets)
-        {
-            sf::Vector2f b_position = b_it.getPosition();
+            for (auto &b_it : bullets)
+            {
+                sf::Vector2f b_position = b_it.getPosition();
+                for (auto &e_it : enemies)
+                {
+                    sf::Vector2f e_position = e_it.getPosition();
+                    if (cal_distant(b_position, e_position) <= 10 * 10)
+                    {
+                        e_it.Heated();
+                        b_it.kill();
+                    }
+                }
+            }
+
             for (auto &e_it : enemies)
             {
                 sf::Vector2f e_position = e_it.getPosition();
-                if ((pow((b_position.x - e_position.x), 2) + pow((b_position.y - e_position.y), 2)) <= 100)
+                sf::Vector2f p_position = p1.getPosition();
+                if (cal_distant(e_position, p_position) <= 20 * 20)
                 {
-                    e_it.Heated();
-                    b_it.kill();
+                    state = game_state::over;
                 }
             }
+
+            for (auto it = enemies.begin(); it != enemies.end();)
+            {
+                if (it->check_if_dead())
+                {
+                    scorer.add(200);
+                    it = enemies.erase(it);
+                }
+                else
+                {
+                    it++;
+                }
+            }
+
+            for (auto it = bullets.begin(); it != bullets.end();)
+            {
+                if (it->check_distance() || !it->check_is_alive())
+                {
+                    it = bullets.erase(it);
+                }
+                else
+                {
+                    it++;
+                }
+            }
+
+            p1.update(dt);
+            // n1.update(dt);
+            
         }
 
-
-        for(auto it = enemies.begin(); it != enemies.end(); ){
-            if(it->check_if_dead()){
-                scorer.add(200);
-                it = enemies.erase(it);
-            }
-            else{
-                it++;
-            }
-
-        }
-
-        bullets.erase(std::remove_if(bullets.begin(), bullets.end(),
-                                     [](Bullet &b)
-                                     { return (b.check_distance() || !b.check_is_alive()); }),
-                      bullets.end());
-
-        p1.update(dt);
-        // n1.update(dt);
         window.clear();
+
+        if (state == game_state::over)
+        {
+        }
 
         // 展示
         for (auto &it : bullets)
